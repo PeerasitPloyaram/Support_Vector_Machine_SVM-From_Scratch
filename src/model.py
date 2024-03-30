a5f7f36195ea94fcc8299b76d4b63b61f0613819
Peerasit <peerasitearth@outlook.com>
2024-03-30 15:37:00 +0700

    Change to Epoch and Test Plot Graph

--- src/model.py
+++ src/model.py
@@ -1,19 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class SVM:
     def __init__(self,kernel='linear', learning_rate=0.001, c=1,lambda_param=0.01 ,max_itr=1000, debug=False, verbose=False)-> None:
         self.learningRate = learning_rate
         self.lambda_param = lambda_param
         self.c = c
-        self.itr = max_itr
+        self.epoch = max_itr
         self.w = None           # Weight
 
         self.debug = debug      # True / False
         self.gradient_round = 0 # Gradient step
         self.verbose = verbose
 
+        self.cost_function = []
+        self.plot = False
+
         if self.debug:
-            print("-- Parameter --\nC: {}\nLearning Rate: {}\nLambda Param: {}\nN_Iters: {}\n---------------".format(self.c, self.learningRate, self.lambda_param, self.itr))
+            print("-- Parameter --\nC: {}\nLearning Rate: {}\nLambda Param: {}\nN_Iters: {}\n---------------".format(self.c, self.learningRate, self.lambda_param, self.epoch))
 
     def add_bias(self, features):
         n_samples = len(features)
@@ -40,18 +44,20 @@ class SVM:
         index, x_sample = x_train.shape
         self.w = np.zeros(x_sample)
 
-        for itr in range(self.itr):
+        for epoch in range(self.epoch):
             for index, x_sample in enumerate(x_train):
                 # Compute Gradient
                 gradient = self.gradient(x_sample, y_train[index])  # Find gradient
 
-                if self.verbose == True:
-                    pass
-                    # print(gradient)
-
                 # Update Weight
                 self.w -= self.learningRate * gradient  # Update new weight
 
+            cost_w = 1 / 2 * np.dot(self.w, self.w) + ( self.lambda_param * (sum(gradient) / len(x_sample)))
+            self.cost_function.append(cost_w)
+
+            if self.verbose == True:
+                print(cost_w)
+            
         if self.debug:
             print("Gradient {} steps.".format(self.gradient_round))
 
@@ -73,3 +79,29 @@ class SVM:
             if sample_test == y_test[index]:    # If equal +1
                 counter += 1
         return counter / len(x_test)
+    
+
+    def plot_cost(self):
+        plt.figure(figsize=(10,5))
+        plt.plot(self.cost_function)
+
+    def plot_accuracy(self, x_train, y_train, x_test, y_test, epoch):
+        l1 = []
+        l2 = []
+        self.debug = False
+        epoch = [1, 10 , 50 ,100 , 500, 1000, 1500,1600,1700,1800,1900,2000]
+        for i in epoch:
+            self.epoch = i
+            model = self.fit(x_train, y_train)
+            score1 = self.score(x_test, y_test)
+            score2 = self.score(x_train, y_train)
+            l1.append(score1)
+            l2.append(score2)
+
+
+        plt.figure(figsize=(10,5))
+        plt.plot(l1, label="Validation", marker='o')
+        plt.plot(l2, label="Train", marker='x')
+        plt.legend()
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
